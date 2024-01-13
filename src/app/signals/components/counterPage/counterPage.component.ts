ae20ee7226d5edbc4a5d89dfa11a0bb94d855751
Daniel Ramirez <dani_0318@hotmail.com>
2024-01-12 22:20:58 -0500

    uso de signals y computed para info de ususario

--- src/app/signals/components/counterPage/counterPage.component.ts
+++ src/app/signals/components/counterPage/counterPage.component.ts
@@ -1,9 +1,17 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
   templateUrl: './counterPage.component.html',
   styleUrl: './counterPage.component.scss',
 })
-export class CounterPageComponent { }
+export class CounterPageComponent { 
+  public counter = signal(10);
+  public squareCounter = computed( ()=> this.counter() * this.counter() );
+
+
+  increaseBy(value: number){
+    this.counter.update(current => current + value);
+  }
+
+}
--- src/app/signals/components/userInfoPage/userInfoPage.component.ts
+++ src/app/signals/components/userInfoPage/userInfoPage.component.ts
@@ -1,9 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, computed, inject, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-user-info-page',
   templateUrl: './userInfoPage.component.html',
   styleUrl: './userInfoPage.component.scss',
 })
-export class UserInfoPageComponent { }
+export class UserInfoPageComponent implements OnInit{
+  
+  private usersService = inject(UsersServiceService);
+  public userId = signal(1);
+
+  public currentUser = signal<User| undefined>(undefined);
+  public userWasFound = signal(true);
+  public fullName = computed<string>(()=>{
+    if(!this.currentUser()) return 'Usuario no encontrado';
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`
+  });
+
+  ngOnInit(): void {
+    this.loadUser( this.userId() );
+  }
+
+  loadUser(id:number){
+    if( id<= 0 ) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined)
+    this.usersService.getUserById(id).subscribe({
+      next:(user)=>{
+        this.currentUser.set( user );
+        this.userWasFound.set(true);
+      },
+      error:() => {
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+      },
+    });
+  }
+
+}
