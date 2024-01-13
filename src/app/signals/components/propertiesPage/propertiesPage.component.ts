9f46f4f15985fd300feafa5aec1690ab64c84a23
Daniel Ramirez <dani_0318@hotmail.com>
2024-01-12 23:13:44 -0500

    ejemplo de effect cuando una señal se cambia

--- src/app/signals/components/propertiesPage/propertiesPage.component.ts
+++ src/app/signals/components/propertiesPage/propertiesPage.component.ts
@@ -1,9 +1,47 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
   templateUrl: './propertiesPage.component.html',
   styleUrl: './propertiesPage.component.scss',
 })
-export class PropertiesPageComponent { }
+export class PropertiesPageComponent {
+
+  public user = signal<User>({
+    "id": 1,
+    "email": "george.bluth@reqres.in",
+    "first_name": "George",
+    "last_name": "Bluth",
+    "avatar": "https://reqres.in/img/faces/1-image.jpg"
+  });
+
+  public userChangedEffect = effect(()=>{
+    console.log(this.user().first_name);
+    
+  });
+
+
+  onFiledUpdated(field: keyof User, value:string){
+
+    this.user.update(current => {
+      switch(field){
+        case 'email':
+          current.email = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+
+      }
+      return current;
+    });
+
+  }
+ }
